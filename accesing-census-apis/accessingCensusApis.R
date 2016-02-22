e778516e8b603931108ea4dd4d9a11289cb84699
Hannah Recht <hrecht@users.noreply.github.com>
2016-02-21 22:16:56 -0500

    use censusapi package

--- accesing-census-apis/accessingCensusApis.R
+++ accesing-census-apis/accessingCensusApis.R
@@ -4,7 +4,7 @@
 # Hannah Recht & Alex Engler
 
 # IMPORTANT: First, request a Census API key here and save the key: http://api.census.gov/data/key_signup.html
-# List of Census datasets available via API: http://api.census.gov/data.html
+# More information: http://www.census.gov/developers/
 ########################################################################################################
 
 # Paste the key that Census sent you - it should be a string of letters and numbers
@@ -13,23 +13,23 @@ censuskey <- "PASTEYOURKEYHERE"
 # View an example response of population and median household income by county from the 2000 decennial Census - paste this url into your web browser:
 # http://api.census.gov/data/2000/sf3?get=NAME,P001001,P053001&for=county:*
 
-# To get API data directly in R, we'll be using a function called getCensusApi, written by Nicholas Nagle - with some additions
-# For more information on the original function and examples, see https://rpubs.com/nnnagle/19337
+# To get API data directly in R, we'll be using the censusapi package
+# Install for the first time
+install.packages("devtools")
+devtools::install_github("hrecht/censusapi")
 
-# We'll need XML installed to be able to read in Census API information tables
-# install.packages("XML")
-
-# 'Source' tells R to run the script, in this case loading the Census API functions
-source("https://raw.githubusercontent.com/UrbanInstitute/R-Trainings/master/accesing-census-apis/getCensusApi.R")
-# getCensusApi function takes four arguments:
-# data-url (root URL of the API, e.g. 'http://api.census.gov/data/2000/sf3')
-# key (your census API key, saved above)
-# vars (a list of variables to get)
-# region (geography to use, e.g. 'county:*')
+# Load the library
+library("censusapi")
 
 # See the available APIs - from http://api.census.gov/data.html
 apis <- listCensusApis()
-head(apis)
+View(apis)
+
+# getCensus function takes four arguments:
+# apiurl (root URL of the API, e.g. 'http://api.census.gov/data/2000/sf3')
+# key (your census API key, saved above)
+# vars (a list of variables to get)
+# region (geography to use, e.g. 'county:*')
 
 ########################################################################################################
 # 2000 decennial census API - we'll use Summary File 3
@@ -40,24 +40,25 @@ head(apis)
 # URL of your API
 sf3_2000_api <- "http://api.census.gov/data/2000/sf3"
 # Let's see what variables are available - from http://api.census.gov/data/2000/sf3/variables.html
-vars2000 <- listCensusVars(sf3_2000_api)
+vars2000 <- listCensusMetadata(sf3_2000_api, "v")
+View(vars2000)
 
 # Variables to get - total population, median household income, median gross rent
 myvars <- c("P001001", "P053001", "H063001")
 
 # Get data at state-level
-data2000 <- getCensusApi(data_url=sf3_2000_api, key=censuskey, vars=myvars, region="state:*")
+data2000 <- getCensus(apiurl=sf3_2000_api, key=censuskey, vars=myvars, region="state:*")
 # View first 6 rows
 head(data2000)
 
 # Let's look at some other geography options
-geos2000 <- listCensusGeos(sf3_2000_api)
+listCensusMetadata(sf3_2000_api, "g")
 
 # Get data at county-level for California
-data2000 <- getCensusApi(data_url=sf3_2000_api, key=censuskey, vars=myvars, region="county:*&in=state:06")
+data2000 <- getCensus(apiurl=sf3_2000_api, key=censuskey, vars=myvars, region="county:*&in=state:06")
 head(data2000)
 # Get data at county-level for all states
-data2000 <- getCensusApi(data_url=sf3_2000_api, key=censuskey, vars=myvars, region="county:*")
+data2000 <- getCensus(apiurl=sf3_2000_api, key=censuskey, vars=myvars, region="county:*")
 head(data2000)
 
 # Plot median rent vs median household income
@@ -77,17 +78,19 @@ acs_2014_api <- 'http://api.census.gov/data/2014/acs5'
 myvars <- c("B01001_001E", "B19013_001E", "B17010_017E", "B17010_037E")
 
 # Let's look at some other geography options - lots available for the ACS
-geos2014 <- listCensusGeos(acs_2014_api)
-# Get all PUMAs in California
-data2014 <- getCensusApi(data_url=acs_2014_api, key=censuskey, vars=myvars, region="public+use+microdata+area:*&in=state:06")
+geos2014 <- listCensusMetadata(acs_2014_api, type="g")
+View(geos2014)
+
+# Get data for all PUMAs in California
+data2014 <- getCensus(apiurl=acs_2014_api, key=censuskey, vars=myvars, region="public+use+microdata+area:*&in=state:06")
 head(data2014)
 
 # Congressional districts
-data2014 <- getCensusApi(data_url=acs_2014_api, key=censuskey, vars=myvars, region="congressional+district:*")
+data2014 <- getCensus(apiurl=acs_2014_api, key=censuskey, vars=myvars, region="congressional+district:*")
 head(data2014)
 
-# Get data for all counties
-data2014 <- getCensusApi(data_url=acs_2014_api, key=censuskey, vars=myvars, region="county:*")
+# All counties
+data2014 <- getCensus(apiurl=acs_2014_api, key=censuskey, vars=myvars, region="county:*")
 head(data2014)
 
 # Calculate % of families with kids under 18 led by single moms that are living below the poverty line
