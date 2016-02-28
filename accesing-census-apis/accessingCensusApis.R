05c41170cf5b8e3df72c60da1aba59f0d07f6460
Hannah Recht <hrecht@users.noreply.github.com>
2016-02-28 17:44:14 -0500

    update for latest censusapi syntax

--- accesing-census-apis/accessingCensusApis.R
+++ accesing-census-apis/accessingCensusApis.R
@@ -25,72 +25,40 @@ library("censusapi")
 apis <- listCensusApis()
 View(apis)
 
-# getCensus function takes four arguments:
-# apiurl (root URL of the API, e.g. 'http://api.census.gov/data/2000/sf3')
+# getCensus function takes five main arguments:
+# name (name of the API, e.g. 'sf3' or 'acs5')
+# vintage (year, e.g. 2014, required for non-timeseries APIs)
 # key (your census API key, saved above)
 # vars (a list of variables to get)
 # region (geography to use, e.g. 'county:*')
 
 ########################################################################################################
-# 2000 decennial census API - we'll use Summary File 3
-# General information: http://www.census.gov/data/developers/data-sets/decennial-census-data.html
-# 2000 decennial datasets: http://api.census.gov/data/2000.html
-# "Summary File 3 consists of 813 detailed tables of Census 2000 social, economic and housing characteristics compiled from a sample of approximately 19 million housing units (about 1 in 6 households) that received the Census 2000 long-form questionnaire."
-########################################################################################################
-# URL of your API
-sf3_2000_api <- "http://api.census.gov/data/2000/sf3"
-# Let's see what variables are available - from http://api.census.gov/data/2000/sf3/variables.html
-vars2000 <- listCensusMetadata(sf3_2000_api, "v")
-View(vars2000)
-
-# Variables to get - total population, median household income, median gross rent
-myvars <- c("P001001", "P053001", "H063001")
-
-# Get data at state-level
-data2000 <- getCensus(apiurl=sf3_2000_api, key=censuskey, vars=myvars, region="state:*")
-# View first 6 rows
-head(data2000)
-
-# Let's look at some other geography options
-listCensusMetadata(sf3_2000_api, "g")
-
-# Get data at county-level for California
-data2000 <- getCensus(apiurl=sf3_2000_api, key=censuskey, vars=myvars, region="county:*&in=state:06")
-head(data2000)
-# Get data at county-level for all states
-data2000 <- getCensus(apiurl=sf3_2000_api, key=censuskey, vars=myvars, region="county:*")
-head(data2000)
-
-# Plot median rent vs median household income
-plot(data2000$P053001, data2000$H063001)
-
-########################################################################################################
 # ACS most recent 5-year data - 2010-14
 # General information: http://www.census.gov/data/developers/data-sets/acs-survey-5-year-data.html
-# List of available variables: http://api.census.gov/data/2014/acs5/variables.html
 
 # Let's get some very specific variables - what % of families with kids under 18 led by single moms are living below the poverty line?
 # Variable info: http://api.census.gov/data/2014/acs5/variables/B17010_017E.json, http://api.census.gov/data/2014/acs5/variables/B17010_037M.json
 ########################################################################################################
-# URL of your API
-acs_2014_api <- 'http://api.census.gov/data/2014/acs5'
+# Look at our geography options - lots available for the ACS
+geos2014 <- listCensusMetadata(name="acs5", vintage=2014,  type="g")
+View(geos2014)
+
+# See list of variables available - for some APIs there are only a few, for ACS there are tens of thousands
+# This is the data from http://api.census.gov/data/2014/acs5/variables.html
+# Not running in training session unless Internet is fast
+# vars2014 <- listCensusMetadata(name="acs5", vintage=2014, "v")
+# View(vars2014)
+
 # Variables to get - total population, median household income, families w/ kids led by single moms who are below poverty, families w/ kids led by single moms who are at or above poverty
 myvars <- c("B01001_001E", "B19013_001E", "B17010_017E", "B17010_037E")
 
-# Let's look at some other geography options - lots available for the ACS
-geos2014 <- listCensusMetadata(acs_2014_api, type="g")
-View(geos2014)
-
-# Get data for all PUMAs in California
-data2014 <- getCensus(apiurl=acs_2014_api, key=censuskey, vars=myvars, region="public+use+microdata+area:*&in=state:06")
-head(data2014)
-
-# Congressional districts
-data2014 <- getCensus(apiurl=acs_2014_api, key=censuskey, vars=myvars, region="congressional+district:*")
+# Geography note: "*" means all available - so region="state:*" means all available states
+# Get data for all congressional districts
+data2014 <- getCensus(name="acs5", vintage=2014, key=censuskey, vars=myvars, region="congressional district:*")
 head(data2014)
 
 # All counties
-data2014 <- getCensus(apiurl=acs_2014_api, key=censuskey, vars=myvars, region="county:*")
+data2014 <- getCensus(name="acs5", vintage=2014, key=censuskey, vars=myvars, region="county:*")
 head(data2014)
 
 # Calculate % of families with kids under 18 led by single moms that are living below the poverty line
@@ -114,3 +82,53 @@ ggplot(data=data2014, aes(x=fampov)) + geom_histogram(binwidth=.02, color="black
 # Boxplot by state for MD, VA, WV
 mvw <- data2014 %>% filter(state %in% c("24", "51", "54"))
 ggplot(data=mvw, aes(x=state, y=fampov)) + geom_boxplot()
+
+########################################################################################################
+# Advanced functions
+########################################################################################################
+
+# Get data within a specified state (fips code 06 = California)
+data2000 <- getCensus(name="sf3", vintage=2000, key=censuskey, vars=c("P001001", "P053001", "H063001"), region="county:*", regionin="state:06")
+head(data2000)
+
+data2014 <- getCensus(name="acs5", vintage=2014, key=censuskey, vars=c("B01001_001E", "B19013_001E", "B17010_017E", "B17010_037E"), region="public use microdata area:*", regionin="state:06")
+head(data2014)
+
+# Loop over all states for small geographies that are nested under state-level geography (e.g. tract)
+# Note: this might take a few minutes depending on your Internet speed
+tracts <- NULL
+for (f in fips) {
+	stateget <- paste("state:", f, sep="")
+	temp <- getCensus(name="acs5", vintage=2014, key=censuskey, vars=c("B01001_001E", "B19013_001E", "B17010_017E", "B17010_037E"), region="tract:*", regionin=stateget)
+	tracts <- rbind(tracts, temp)
+}
+head(tracts)
+
+########################################################################################################
+# Additional example
+# 2000 decennial census API - we'll use Summary File 3
+# General information: http://www.census.gov/data/developers/data-sets/decennial-census-data.html
+# 2000 decennial datasets: http://api.census.gov/data/2000.html
+# "Summary File 3 consists of 813 detailed tables of Census 2000 social, economic and housing characteristics compiled from a sample of approximately 19 million housing units (about 1 in 6 households) that received the Census 2000 long-form questionnaire."
+########################################################################################################
+# Let's see what variables are available - from http://api.census.gov/data/2000/sf3/variables.html
+vars2000 <- listCensusMetadata(name="sf3", vintage=2000, "v")
+View(vars2000)
+
+# Variables to get - total population, median household income, median gross rent
+myvars <- c("P001001", "P053001", "H063001")
+
+# Get data at state-level
+data2000 <- getCensus(name="sf3", vintage=2000, key=censuskey, vars=myvars, region="state:*")
+# View first 6 rows
+head(data2000)
+
+# Let's look at some other geography options
+listCensusMetadata(name="sf3", vintage=2000, "g")
+
+# Get data at county-level for all states
+data2000 <- getCensus(name="sf3", vintage=2000, key=censuskey, vars=myvars, region="county:*")
+head(data2000)
+
+# Plot median rent vs median household income
+plot(data2000$P053001, data2000$H063001)
