d338950530844ec686228bb7f1538c3b1c8099bd
alexcengler <alexcengler@gmail.com>
2016-09-09 13:27:52 -0400

    Updates, fixing minor errors

--- intro-to-statistical-learning/Statistical Learning.R
+++ intro-to-statistical-learning/Statistical Learning.R
@@ -1,4 +1,4 @@
-#### Introdcution to R for Statistical Learning 
+#### Introduction to R for Statistical Learning 
 #### Urban Institute Training
 ### Alex C. Engler
 ### 9/9/2016
@@ -16,17 +16,20 @@
 # 
 # Wonderful textbook for 'statistical learning' - which includes not only statistics, but also many of the foundational skills in machine learning and data science. Nine out of ten of these chapters also have an R tutorial at the end. 
 
+## R for Data Science - Chapters 23/24/25: http://r4ds.had.co.nz/model-basics.html#visualising-models
+# This new free ebook goes into depth explaining R models and shows a wide variety of tricks to analyze and visualize those models. 
+
 ##############################################################
 
 ## You should remove the '#' and install the packages below, if you do not have them already:
 # install.packages("dplyr")
 # install.packages("tidyr")
+# install.packages("ggplot2")
 
 library(dplyr) 
 ## dplyr 
 # Convenient easy functions for subseting, reordering, adding columns, simple aggregations, simple random sampling
 # Introduction here: https://cran.rstudio.com/web/packages/dplyr/vignettes/introduction.html
-
 
 library(tidyr)
 ## tidyr
@@ -39,8 +42,10 @@ library(tidyr)
 diamonds <- read.csv(file.choose(), header=TRUE)
 
 
+
+## Three ways to look at the data:
 head(diamonds) # See first six rows.
-glimpse(diamonds) # Rotated display of the data (looks cleaner)
+glimpse(diamonds) # Rotated display of the data (from dplyr package)
 str(diamonds) # See data structure
 
 
@@ -62,23 +67,33 @@ new_data2 <- diamonds %>%
 new_data == new_data2
 table(new_data == new_data2)
 
-
-## dplyr's chain operators work into ggplot2 as well:
-
-library(ggplot2)
-diamonds %>%
-	filter(cut == "Ideal") %>%
-	group_by(channel) %>%
-	summarize(avg_price = mean(price)) %>%
-	arrange(-avg_price) %>%
-	ggplot(aes(x=channel, y=avg_price)) + geom_bar(stat="identity")
-
 rm(new_data, new_data2) ## delete these dataframes
 
 
+## dplyr's chain operators work into ggplot2 as well:
+library(ggplot2)
+
+diamonds %>%
+	group_by(store) %>%
+	summarize(avg_price = mean(price),
+		med_carat = median(carat),
+		count = n()) %>%
+	filter(count > 10) %>%
+	arrange(-avg_price) %>%
+	ggplot(aes(x=med_carat, y=avg_price, color=store, size=count)) + 
+		geom_point() + 
+		ggtitle("Diamond Stores by Carat & Cost") +
+		theme_bw()
+
 
 ## By default, R considers strings to be factors (R's categorical variables) upon loading. We can change the data type back to a string easily.
+class(diamonds$store)
 diamonds$store <- as.character(diamonds$store)
+class(diamonds$store)
+
+## Note you can also run:
+# options(stringsAsFactors=FALSE)
+## at the start of an R session, which will undo this default.
 
 ## Handy Descriptive Statistics:
 mean(diamonds$price)
@@ -88,6 +103,7 @@ fivenum(diamonds$price)
 
 quantile(diamonds$price, 0.33)
 quantile(diamonds$price, c(0.33,0.66))
+quantile(diamonds$price, c(0.2,0.4,0.6,0.8))
 
 cor(diamonds$carat, diamonds$price) ## correlation
 cov(diamonds$carat, diamonds$price) ## covariance
@@ -100,7 +116,6 @@ sapply(diamonds, sd)
 
 
 ## Simple linear regression use lm()
-
 linear_model <- lm(price ~ carat, data = diamonds)
 
 linear_model
@@ -111,6 +126,8 @@ attributes(linear_model)
 # You can refer to individual attributes with the '$'
 linear_model$call
 linear_model$fitted.values
+
+plot(linear_model$fitted.values, linear_model$residuals)
 
 
 ## Multivariate Linear Regression
@@ -124,8 +141,6 @@ summary(linear_model2)
 plot(linear_model2)
 
 
-## More regression diagnostics:
-plot(linear_model2$fitted.values, linear_model2$residuals)
 
 # QQ Plots
 qqnorm(linear_model2$residuals)
@@ -135,9 +150,13 @@ hist(linear_model2$residuals)
 ## Leverage and Outliers 
 ?influence.measures
 hatvalues(linear_model2) ## check for leverage
-cook_list <- cooks.distance(linear_model2) ## check for high influence 
-cook_list[cook_list > 0.05]
 
+cook_list <- cooks.distance(linear_model2) ## check for high influence 
+outliers <- cook_list[cook_list > 0.05] 
+
+## To look at the rows of data with high cooks distance:
+# names(outliers)
+# diamonds[names(outliers),]
 
 # Interaction Effects with *
 linear_model3 <- lm(price ~ carat*color + clarity + cut + channel, data = diamonds) 
@@ -147,28 +166,36 @@ summary(linear_model3)
 linear_model4 <- lm(price ~ carat*color - color + clarity + cut + channel, data = diamonds) 
 summary(linear_model4)
 
+# Weighting with "weight" argument:
+linear_model5 <- lm(price ~ color + clarity + cut + channel, weight=carat, data = diamonds) 
+summary(linear_model5)
+
+
 
 ## Analysis of Variance with anova()
+# Performs chi-square test to check significance in RSS 
 anova(linear_model2, linear_model3)
 anova(linear_model, linear_model2, linear_model3)
 
 
-
-
 ## Generalized Linear Models use glm()
 # GLM Overview: http://www.statmethods.net/advstats/glm.html
+str(diamonds$cut)
+table(diamonds$cut)
 
 logistic_model <- glm(cut ~ price + carat + clarity + channel, data = diamonds, family = binomial)
+summary(logistic_model)
+
 logistic_model$coefficients
 exp(logistic_model$coefficients)
 
-poisson_model <- glm(clarity ~ color + cut + channel, data = diamonds, family = "poisson")
+## Can reoder factor if you prefer:
+# diamonds$cut <- factor(diamonds$cut, levels=c("Not Ideal","Ideal"))
 
 
 ## CAR - Companion to Applied Regression
 ## install.packages("car")
 library(car)
-?car ## Look at documentation (only works after you load library)
 
 outlierTest(linear_model2) ## outlier tests using Bonferonni p-value
 vif(linear_model2) ## Variance Influence Factors
@@ -177,7 +204,7 @@ vif(linear_model2) ## Variance Influence Factors
 ## MASS - Modern Applied Statistics
 
 library(MASS) 
-r_lm <- rlm(price ~ carat + color + clarity + cut + channel, data = diamonds) ## robust linear regression
+r_lm <- rlm(price ~ carat + color + clarity + cut + channel, data = diamonds) ## Robust Linear Regression w/ IRLS
 attributes(r_lm)
 
 
