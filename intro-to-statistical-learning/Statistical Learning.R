803bc8ad70bd5c39112e6759d0ab5e93125e361b
alexcengler <alexcengler@gmail.com>
2017-04-28 16:16:38 -0400

    Updated stat learning

--- intro-to-statistical-learning/Statistical Learning.R
+++ intro-to-statistical-learning/Statistical Learning.R
@@ -1,7 +1,7 @@
 #### Introduction to R for Statistical Learning 
 #### Urban Institute Training
 ### Alex C. Engler
-### 9/9/2016
+### 4/27/2017
 
 ##############################################################
 
@@ -23,56 +23,17 @@
 
 ## You should remove the '#' and install the packages below, if you do not have them already:
 # install.packages("dplyr")
-# install.packages("tidyr")
 # install.packages("ggplot2")
 
 library(dplyr) 
-## dplyr 
-# Convenient easy functions for subseting, reordering, adding columns, simple aggregations, simple random sampling
-# Introduction here: https://cran.rstudio.com/web/packages/dplyr/vignettes/introduction.html
-
-library(tidyr)
-## tidyr
-# Wide >> Long with gather()
-# Long >> Wide with spread()
-# Introduction here: http://blog.rstudio.org/2014/07/22/introducing-tidyr/
-
-## General guide to tidyr and dplyr: https://rpubs.com/bradleyboehmke/data_wrangling
-
-diamonds <- read.csv(file.choose(), header=TRUE)
-
-
-
-## Three ways to look at the data:
-head(diamonds) # See first six rows.
-glimpse(diamonds) # Rotated display of the data (from dplyr package)
-str(diamonds) # See data structure
-
-
-## dplyr example:
-
-# Simple functions for data manipulation:
-new_data <- filter(diamonds, cut == "Ideal")
-new_data <- group_by(new_data, channel)
-new_data <- summarize(new_data, avg_price = mean(price))
-new_data <- arrange(new_data, -avg_price)
-
-# Can chain those operations together with the %>% operator:
-new_data2 <- diamonds %>%
-	filter(cut == "Ideal") %>%
-	group_by(channel) %>%
-	summarize(avg_price = mean(price)) %>%
-	arrange(-avg_price)
-
-new_data == new_data2
-table(new_data == new_data2)
-
-rm(new_data, new_data2) ## delete these dataframes
-
-
-## dplyr's chain operators work into ggplot2 as well:
 library(ggplot2)
 
+## Read in our sample dataset:
+diamonds <- read.csv("diamonds.csv", header=TRUE)
+
+glimpse(diamonds)
+
+## dplyr & ggplot refresher:
 diamonds %>%
 	group_by(store) %>%
 	summarize(avg_price = mean(price),
@@ -85,11 +46,9 @@ diamonds %>%
 		ggtitle("Diamond Stores by Carat & Cost") +
 		theme_bw()
 
-
 ## By default, R considers strings to be factors (R's categorical variables) upon loading. We can change the data type back to a string easily.
 class(diamonds$store)
 diamonds$store <- as.character(diamonds$store)
-class(diamonds$store)
 
 ## Note you can also run:
 # options(stringsAsFactors=FALSE)
@@ -101,81 +60,139 @@ median(diamonds$price)
 sd(diamonds$price)
 fivenum(diamonds$price)
 
+## Quantiles:
 quantile(diamonds$price, 0.33)
 quantile(diamonds$price, c(0.33,0.66))
 quantile(diamonds$price, c(0.2,0.4,0.6,0.8))
 
+## Correlation & Covariance
 cor(diamonds$carat, diamonds$price) ## correlation
 cov(diamonds$carat, diamonds$price) ## covariance
 
-## apply family of functions (more here: http://faculty.nps.edu/sebuttre/home/R/apply.html)
-sapply(diamonds, mean)
-sapply(diamonds, sd)
+## Scatterplot:
+ggplot(diamonds, aes(x=carat, y=price)) + 
+  geom_point() +
+  theme_bw()
 
-
+## apply family of functions (more here: http://faculty.nps.edu/sebuttre/home/R/apply.html). Sapply applies  
+sapply(diamonds[,c(1,2,3,7)], mean)
+sapply(diamonds[,c(1,2,3,7)], sd)
+sapply(diamonds[,c(1,2,3,7)], quantile, c(0.2, 0.4, 0.6, 0.8))
 
 
 ## Simple linear regression use lm()
+## lm(formula, dataframe)
 linear_model <- lm(price ~ carat, data = diamonds)
 
+## Quick Overview:
 linear_model
-summary(linear_model) ## Quick Overview
+summary(linear_model)
 
+## Accessing specific model information:
+class(linear_model)
 attributes(linear_model)
 
-# You can refer to individual attributes with the '$'
+# You can refer to individual attributes with the '$' operator:
 linear_model$call
+linear_model$coefficients
 linear_model$fitted.values
 
-plot(linear_model$fitted.values, linear_model$residuals)
+
+## Plot the fitted model:
+coefs <- linear_model$coefficients
+
+ggplot(data=diamonds) + 
+  geom_point(aes(x=carat, y=price), alpha=0.6) +
+  geom_abline(aes(intercept=coefs[1], slope=coefs[2])) + 
+  theme_bw()
+
+## Combine fitted values and our original dataset:
+diamonds_more <- bind_cols(diamonds, as.data.frame(linear_model$fitted.values))
+glimpse(diamonds_more)
+colnames(diamonds_more)[8] <- "fitted_values"
+
+## Plotting residuals with ggplot2:
+ggplot(data=diamonds_more) + 
+  geom_point(aes(x=carat, y=price), alpha=0.6) +
+  geom_abline(aes(intercept=coefs[1], slope=coefs[2])) + 
+  geom_segment(aes(x=carat, xend=carat, y=price, yend=fitted_values), color="#1696d3") +
+  theme_bw()
 
 
-## Multivariate Linear Regression
-linear_model2 <- lm(price ~ ., data = diamonds) ## Regress against all vars in dataframe
 
-linear_model2 <- lm(price ~ . -store, data = diamonds) ## Regress against all vars in dataframe, except for 'store'
+## Multivariate Linear Regression with R Formula Syntax:
+### Notes of R forumla syntax: http://faculty.chicagobooth.edu/richard.hahn/teaching/formulanotation.pdf
 
-linear_model2 <- lm(price ~ carat + color + clarity + cut + channel, data = diamonds) ## Or specify variables to be included
+## . signifies all vars in dataframe:
+linear_model <- lm(price ~ ., data = diamonds) 
+summary(linear_model)
 
-summary(linear_model2)
-plot(linear_model2)
+## - signifies removing this variable:
+linear_model <- lm(price ~ . -store, data = diamonds)
+summary(linear_model)
+
+## Or you can use + to specify the list of variables you want to include:
+linear_model <- lm(price ~ carat + color + clarity + cut + channel, data = diamonds)
+summary(linear_model)
+
+## Note how the modeling treats factors, the first level is left out:
+linear_model$coefficients
+levels(diamonds$cut)
+levels(diamonds$channel)
+
+## You can reoder factor if you prefer a different value left out:
+diamonds$cut <- factor(diamonds$cut, levels=c("Not Ideal","Ideal"))
 
 
+## : signifies an interaction between two variables:
+linear_model <- lm(price ~ carat + clarity:cut, data = diamonds) 
+summary(linear_model)
+
+## * signifies an interaction between two variables AND those variables independently:
+linear_model <- lm(price ~ carat + clarity*cut, data = diamonds) 
+summary(linear_model)
+
+## You can manipulate variables in the forumla, too:
+linear_model <- lm(price ~ carat + sqrt(carat), data = diamonds) 
+summary(linear_model)
+
+
+## There is a 'weights' argument for most models that can use them:
+linear_model <- lm(price ~ color + clarity + cut + channel, weight=carat, data = diamonds) 
+summary(linear_model)
+
+
+## Missing data - R will silently drop rows with missing data (if that column is in regression):
+diamonds_tmp <- diamonds
+diamonds_tmp$carat[5] <- NA
+model <- lm(price ~ carat, data = diamonds_tmp) 
+
+nrow(diamonds)
+nobs(model)
+
+
+if(nrow(data) != nobs(model)){print("Problem!")}
+
+## the tidy() function in the broom package is useful for storing model results:
+# install.packages("broom")
+library(broom)
+tidy_model <- tidy(linear_model)
+class(tidy_model)
+
+tidy(linear_model)
 
 # QQ Plots
 qqnorm(linear_model2$residuals)
 qqline(linear_model2$residuals)
-hist(linear_model2$residuals) 
-
-## Leverage and Outliers 
-?influence.measures
-hatvalues(linear_model2) ## check for leverage
-
-cook_list <- cooks.distance(linear_model2) ## check for high influence 
-outliers <- cook_list[cook_list > 0.05] 
-
-## To look at the rows of data with high cooks distance:
-# names(outliers)
-# diamonds[names(outliers),]
-
-# Interaction Effects with *
-linear_model3 <- lm(price ~ carat*color + clarity + cut + channel, data = diamonds) 
-summary(linear_model3)
-
-# Interaction Effects with * and -
-linear_model4 <- lm(price ~ carat*color - color + clarity + cut + channel, data = diamonds) 
-summary(linear_model4)
-
-# Weighting with "weight" argument:
-linear_model5 <- lm(price ~ color + clarity + cut + channel, weight=carat, data = diamonds) 
-summary(linear_model5)
-
-
+hist(linear_model2$residuals, breaks=30) 
+## Basics on interpreting a qqplot: https://stats.stackexchange.com/questions/101274/how-to-interpret-a-qq-plot
 
 ## Analysis of Variance with anova()
 # Performs chi-square test to check significance in RSS 
-anova(linear_model2, linear_model3)
-anova(linear_model, linear_model2, linear_model3)
+linear_model1 <- lm(price ~ carat + sqrt(carat), data = diamonds) 
+linear_model2 <- lm(price ~ carat + log(carat) + cut, data = diamonds) 
+
+anova(linear_model, linear_model2)
 
 
 ## Generalized Linear Models use glm()
@@ -185,29 +202,45 @@ table(diamonds$cut)
 
 logistic_model <- glm(cut ~ price + carat + clarity + channel, data = diamonds, family = binomial)
 summary(logistic_model)
+?family
 
+## Exponentiate coefficients to get odds ratio:
 logistic_model$coefficients
 exp(logistic_model$coefficients)
+## Interpreting odds-ratio: http://www.appstate.edu/~whiteheadjc/service/logit/intro.htm#interp
 
-## Can reoder factor if you prefer:
-# diamonds$cut <- factor(diamonds$cut, levels=c("Not Ideal","Ideal"))
+
+
+
+## Leverage and Outliers 
+?influence.measures
+hatvalues(linear_model) ## check for leverage
+
+cook_list <- cooks.distance(linear_model2) ## check for high influence 
+outliers <- cook_list[cook_list > 0.05] 
+
+## To look at the rows of data with high cooks distance:
+names(outliers)
+diamonds[names(outliers),]
 
 
 ## CAR - Companion to Applied Regression
 ## install.packages("car")
 library(car)
-
-outlierTest(linear_model2) ## outlier tests using Bonferonni p-value
-vif(linear_model2) ## Variance Influence Factors
-
+outlierTest(linear_model) ## outlier tests using Bonferonni p-value
+vif(linear_model) ## Variance Influence Factors
 
 ## MASS - Modern Applied Statistics
-
 library(MASS) 
-r_lm <- rlm(price ~ carat + color + clarity + cut + channel, data = diamonds) ## Robust Linear Regression w/ IRLS
+## Robust Linear Regression w/ IRLS
+r_lm <- rlm(price ~ carat + color + clarity + cut + channel, data = diamonds) 
 attributes(r_lm)
 
 
+## Penalized methods with glmnet
+library(glmnet)
+ridge <- glmnet(x=as.matrix(diamonds[,1:3]), y=diamonds$price)
+attributes(ridge)
 
 ## Caret Package - For Applied Machine Learning 
 # install.packages("caret")
@@ -215,5 +248,4 @@ library(caret)
 
 rf_model <- train(price ~., data = diamonds, method="rf")
 nnet_model <- train(price ~., data = diamonds, method="nnet")
-
 ## Model training with caret: http://topepo.github.io/caret/training.html
